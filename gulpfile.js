55f2f9de71b40c565827a9996374aa3091455ecc
RobertSmith-REISYS <robert.smith@reisys.com>
2015-12-01 10:11:10 -0500

    Updated Gulpfile and Build configurations

--- gulpfile.js
+++ gulpfile.js
@@ -10,26 +10,18 @@ var gzip           = require('gulp-gzip');
 var clone          = require('gulp-clone');
 var rename         = require('gulp-rename');
 var series         = require('stream-series');
-var intercept      = require('gulp-intercept');
-var gulpNgConfig   = require('gulp-ng-config');
-var es             = require('event-stream');
 
 var appFilesBase = "src/main/resources/app";
 
 var vendorJs;
 var vendorCss;
 var appJs;
-var appCss;
 var appSass;
 
 gulp.task('vendor-js-files', function () {
     vendorJs = gulp.src(mainBowerFiles('**/*.js'),{ base: 'bower_components' })
         .pipe(concat('lib.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('target/classes/static/vendor/js'));
-
-    vendorJs.pipe(clone())
-        .pipe(gzip())
         .pipe(gulp.dest('target/classes/static/vendor/js'));
 });
 
@@ -38,61 +30,28 @@ gulp.task('vendor-css-files', function () {
         .pipe(concat('lib.min.css'))
         .pipe(minify())
         .pipe(gulp.dest('target/classes/static/vendor/css'));
-
-    vendorCss.pipe(clone())
-        .pipe(clone())
-        .pipe(gzip())
-        .pipe(gulp.dest('target/classes/static/vendor/css'));
 });
 
 gulp.task('app-js-files', function () {
-    // Read application.settings files and add public variables as Angular Constants
-    var appSettings = gulp.src('src/main/resources/application.properties')
-        .pipe(intercept(function(file){
-            console.log(file.path);
-            var jsonObj = {},
-                variables = file.contents.toString().split('\r\n'),
-                varName = 0,
-                varVal = 1;
-
-            variables.forEach(function(variable) {
-                var parts = variable.split('='),
-                    name = parts[varName];
-                if(name.indexOf('pub.') === 0)
-                    jsonObj[name] = parts[varVal];
-            })
-            file.contents = new Buffer(JSON.stringify(jsonObj));
-            return file;
-        }))
-        .pipe(gulpNgConfig('app.settings'))
-        .pipe(rename("app.settings.js"));
-
     //Read App JS files and combine
-    var appJsSrc = gulp.src([
+    appJs = gulp.src([
             appFilesBase + '/*.module.js',
             appFilesBase + '/services/**/*.js',
             appFilesBase + '/**/*.js'
         ], {base: appFilesBase})
-        .pipe(concat('app.src.js'));
-
-    //Combine app.settings.js and app.src.js into one JS file called app.js
-    // and output into the file system.
-    appJs = es.concat(appSettings, appJsSrc)
         .pipe(concat('app.js'))
-        .pipe(gulp.dest('target/classes/static/js'));
-
-    appJs.pipe(clone())
-        .pipe(gzip())
         .pipe(gulp.dest('target/classes/static/js'));
 });
 
-gulp.task('app-css-files', function() {
-    appCss = gulp.src('src/main/resources/static/css/**/*.css')
-        .pipe(gulp.dest('target/classes/static/css'));
+gulp.task('app-static-files', function() {
+    gulp.src('src/main/resources/static/**/*.*')
+        .pipe(gulp.dest('target/classes/static'));
+    gulp.src('src/main/resources/*.*')
+        .pipe(gulp.dest('target/classes'));
 })
 
 gulp.task('app-sass-files', function() {
-    appSass = gulp.src('src/main/resources/static/sass/**/*.scss')
+    appSass = gulp.src('src/main/resources/sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('target/classes/static/css'));
 });
@@ -101,25 +60,24 @@ gulp.task('app-html-tpl-files', function() {
     gulp.src(appFilesBase + "/**/*.tpl.html")
         .pipe(gulp.dest('target/classes/static/partials'));
 });
-gulp.task('app-img-files', function() {
-    gulp.src('src/main/resources/static/img/**/*.*')
-        .pipe(gulp.dest('target/classes/static/img'));
-});
 gulp.task('index', function () {
     var target = gulp.src("src/main/resources/static/index.html");
     var sources = gulp.src(['target/classes/static/*.js', 'target/classes/static/*.css'], {read: false});
     return target.pipe(rename("index.html"))
         .pipe(gulp.dest('target/classes/static'))
-        .pipe(inject(series(vendorJs, vendorCss, appJs, appCss, appSass, sources), {relative: true}))
+        .pipe(inject(series(vendorJs), { relative: true, starttag: '<!-- inject:vendor:{{ext}} -->'}))
+        .pipe(inject(series(vendorCss, appJs, appSass, sources), {relative: true}))
         .pipe(gulp.dest('target/classes/static'));
 });
 
-gulp.task('copyFonts', function() {
-    gulp.src('src/main/resources/static/fonts/*.{ttf,woff,woff2,eof,svg}')
-        .pipe(gulp.dest('target/classes/static/fonts'));
-});
+gulp.task('gzip', function() {
+    gulp.src('target/classes/static/**/*.{js,css}')
+        .pipe(clone())
+        .pipe(gzip())
+        .pipe(gulp.dest('target/classes/static'));
+})
 
 // Default Task
 gulp.task('default', function () {
-    runSequence('vendor-js-files', 'vendor-css-files', 'app-js-files', 'app-css-files', 'app-sass-files', 'app-html-tpl-files', 'app-img-files', 'index', 'copyFonts');
+    runSequence('app-static-files', 'vendor-js-files', 'vendor-css-files', 'app-js-files', 'app-sass-files', 'app-html-tpl-files', 'index', 'gzip');
 });
