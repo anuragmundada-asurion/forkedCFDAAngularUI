c4f198706f12895f24884659221481e5731bb817
RobertSmith-REISYS <robert.smith@reisys.com>
2015-12-02 10:26:05 -0500

    Added UUID generation

---
+++ src/main/resources/app/services/uuid.service.js
@@ -0,0 +1,44 @@
+(function() {
+    "use strict"
+
+    angular
+        .module('app')
+        .factory('uuid', uuidSvc);
+
+    uuidSvc.$inject = ['$window']
+    ////////////////
+
+    function uuidSvc($window) {
+        var crypto = $window.crypto || $window.msCrypto,
+            hasCrypto = angular.isDefined(typeof(crypto)) && angular.isDefined(typeof(crypto.getRandomValues));
+
+        var svc = {
+            generateUUID: hasCrypto ? generateUUID : generateUUIDPolyfill
+        }
+
+        return svc;
+
+        ////////////////////
+
+        function generateUUID() {
+            var buf = new Uint16Array(8);
+            crypto.getRandomValues(buf);
+            return (S4(buf[0])+S4(buf[1])+"-"+S4(buf[2])+"-"+S4(buf[3])+"-"+S4(buf[4])+"-"+S4(buf[5])+S4(buf[6])+S4(buf[7]));
+        }
+
+        function S4(num) {
+            var ret = num.toString(16);
+            while(ret.length < 4){
+                ret = "0"+ret;
+            }
+            return ret;
+        }
+
+        function generateUUIDPolyfill() {
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+                return v.toString(16);
+            });
+        }
+    }
+})();
