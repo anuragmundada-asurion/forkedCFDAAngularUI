7e7f01dfa081113d8b10237900cc61d39ed9e6e1
RobertSmith-REISYS <robert.smith@reisys.com>
2015-12-08 20:53:13 -0500

    Added 'fixes' to the input directive in order to store the value of the input despite validation errors

---
+++ src/main/resources/app/components/fixes.component.js
@@ -0,0 +1,75 @@
+(function() {
+    "use strict";
+
+    angular
+        .module('app')
+        .directive('input', jdFixInvalidValueFormatting)
+        .directive('input', jdFixParsingInconsistencies);
+
+    //////////////////
+
+    function jdFixInvalidValueFormatting() {
+        return {
+            require: '?ngModel',
+            restrict: 'E',
+            link: link
+        };
+
+        function link($scope, $element, $attrs, ngModelController) {
+            var inputType = angular.lowercase($attrs.type);
+
+            if (!ngModelController || inputType === 'radio' ||
+                inputType === 'checkbox') {
+                return;
+            }
+
+            ngModelController.$formatters.unshift(function(value) {
+                if (ngModelController.$invalid && angular.isUndefined(value)
+                    && typeof ngModelController.$modelValue === 'string') {
+                    return ngModelController.$modelValue;
+                } else {
+                    return value;
+                }
+            });
+        }
+    }
+
+    function jdFixParsingInconsistencies() {
+        return {
+            priority: (angular.version.full.indexOf('1.2.') === 0) ? 1 : 0,
+            require: '?ngModel',
+            restrict: 'E',
+            link: link
+        };
+
+        function link($scope, $element, $attrs, ngModelController) {
+            var inputType = angular.lowercase($attrs.type);
+
+            if (!ngModelController || inputType === 'radio' ||
+                inputType === 'checkbox') {
+                return;
+            }
+
+            ngModelController.$parsers.push(fixParser);
+
+            /////////////////
+
+            function fixParser(value) {
+                if ((ngModelController.$invalid)) {
+                    value = ngModelController.$viewValue;
+                    var tempVal;
+                    if(angular.isDefined(value)) {
+                        angular.forEach(ngModelController.$parsers, function ($parser) {
+                            if ($parser !== fixParser) {
+                                tempVal = $parser(value);
+                                if(angular.isDefined(tempVal))
+                                    value = tempVal;
+                            }
+                        });
+                    }
+                }
+                return value;
+            }
+        }
+    }
+})();
