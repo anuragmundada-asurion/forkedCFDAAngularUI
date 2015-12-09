7e5896e0f58a889b4e941ef66642a7d0c7da3d13
ltanguay <lsingh@techflow.com>
2015-12-09 10:57:18 -0500

    Fixed issue in section 2 where multi-select fields weren't displaying values.

--- src/main/resources/app/programs/addedit.controller.js
+++ src/main/resources/app/programs/addedit.controller.js
@@ -24,7 +24,7 @@
                 'program_subject_terms',
                 'assistance_type',
                 'applicant_types',
-                'applicant_usage_types',
+                'assistance_usage_types',
                 'beneficiary_types'
             ];
 
