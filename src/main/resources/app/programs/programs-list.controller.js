0397119808c9531d96b79ae1e001086037e34691
RobertSmith-REISYS <robert.smith@reisys.com>
2015-12-18 11:25:09 -0500

    Added pagination

--- src/main/resources/app/programs/programs-list.controller.js
+++ src/main/resources/app/programs/programs-list.controller.js
@@ -13,6 +13,8 @@
         var vm = this,
             previousState;
 
+        vm.itemsByPage = 10;
+
         vm.loadPrograms = loadPrograms;
         vm.editProgram = editProgram;
         vm.deleteProgram = deleteProgram;
@@ -21,15 +23,26 @@
 
         function loadPrograms(tableState) {
             vm.isLoading = true;
-
+            var queryObj = {
+                limit: vm.itemsByPage,
+                offset: tableState.pagination.start,
+                includeCount: true
+            };
+            if(angular.isDefined(tableState.sort.predicate)) {
+                var isDescending = tableState.sort.reverse,
+                    sortingProperty = tableState.sort.predicate;
+                queryObj.sortBy = ( isDescending ? '-' : '' ) + sortingProperty;
+            }
             console.log(tableState);
 
-            vm.programs = Program.query({
-                limit: 100
-            });
+            vm.programs = Program.query(queryObj);
 
-            vm.programs.$promise.then(function(){
+            vm.programs.$promise.then(function(data){
                 vm.isLoading = false;
+                var totalCount = data.$metadata.totalCount;
+
+                tableState.pagination.numberOfPages = Math.ceil(totalCount / vm.itemsByPage);
+                tableState.pagination.totalItemCount = totalCount;
                 previousState = tableState;
             })
         }
