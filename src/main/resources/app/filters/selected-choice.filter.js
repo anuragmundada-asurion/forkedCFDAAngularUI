7b49ce0d6ceb1c0bb241220e5b8a3564e56a18e6
RobertSmith-REISYS <robert.smith@reisys.com>
2016-01-04 10:05:55 -0500

    Corrected selected choice filter bugs

--- src/main/resources/app/filters/selected-choice.filter.js
+++ src/main/resources/app/filters/selected-choice.filter.js
@@ -14,9 +14,13 @@
         /////////////////
 
         function filterExp(items, selectedChoices, key) {
-            return items.filter(function(item) {
-                return selectedChoices.indexOf(item[key]) != -1
-            });
+            var out = [];
+            if (angular.isArray(items) && angular.isArray(selectedChoices)) {
+                out = items.filter(function (item) {
+                    return selectedChoices.indexOf(item[key]) != -1
+                });
+            }
+            return out;
         }
     }
 })();
