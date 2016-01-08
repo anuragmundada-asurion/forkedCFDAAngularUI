bf0c01db0e847ef3a27ac833ef42b55d61670c3c
RobertSmith-REISYS <robert.smith@reisys.com>
2016-01-08 11:04:22 -0500

    Added the ability to use HTML formated titles for multi-entries

--- src/main/resources/app/components/multi-entry-shell-ui.directive.js
+++ src/main/resources/app/components/multi-entry-shell-ui.directive.js
@@ -48,7 +48,7 @@
                 ctrl.parentVm = newVal;
             });
             if (element.find('multi-entry-list').length <= 0) {
-                var listElement = angular.element("<multi-entry-list></multi-entry-list>"),
+                var listElement = angular.element("<multi-entry-list><span>{{$ctrl.formatTitle(item)}}</span></multi-entry-list>"),
                     filterFunc;
                 listElement.attr('editable', scope.editableItems);
                 listElement.attr('listTrackBy', scope.listTrackBy);
@@ -141,6 +141,7 @@
 
         return {
             restrict: 'E',
+            transclude: true,
             templateUrl: function(element, attr) {
                 return attr.editable === "true"
                     ? 'partials/components/multi-entry-list-editable.tpl.html'
