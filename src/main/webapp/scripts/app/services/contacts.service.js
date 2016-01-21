d56e038c697866f3830a98f15d77e24813c25e38
hieu-trang-reisys <hieu.trang@reisystems.com>
2016-01-21 17:58:40 -0500

    Contact service unit tests. Removed unused code

--- src/main/webapp/scripts/app/services/contacts.service.js
+++ src/main/webapp/scripts/app/services/contacts.service.js
@@ -1,42 +1,24 @@
-(function() {
+(function () {
     "use strict";
 
-    angular
-        .module('app')
-        .factory('Contact', contactSvc);
-
-    contactSvc.$inject = ['$resource', 'env'];
-
-    ////////////////
-
-    function contactSvc($resource, env) {
-        var domainUrl = env["pub.api.programs"] || 'http://gsaiae-dev02.reisys.com:82';
+    angular.module('app').factory('Contact', ['$resource', 'env', function ($resource, env) {
+        var domainUrl = env["pub.api.programs"] || 'http://gsaiae-cfda-program-uat01.reisys.com';
         return $resource(domainUrl + '/contacts/:agencyId', {
             id: '@_id',
             agencyId: '@agencyId'
         }, {
-            save: {
-                method: 'PUT',
-                transformResponse: saveUpdateTransformRes
-            },
-            update: {
-                method: 'POST',
-                params: {
-                    id: '@_id'
-                },
-                transformResponse: saveUpdateTransformRes
-            },
             query: {
                 method: 'GET',
                 isArray: true,
-                transformResponse: function(data){
+                transformResponse: function (data) {
                     data = JSON.parse(jsonEscape(data));
                     var res = data.results,
                         list = [];
-                    angular.forEach(res, function(item){
-                        angular.forEach(item, function(prop, key){
-                            if(!prop._id)
+                    angular.forEach(res, function (item) {
+                        angular.forEach(item, function (prop, key) {
+                            if (!prop._id) {
                                 prop._id = key;
+                            }
                             list.push(prop);
                         })
                     });
@@ -53,31 +35,11 @@
                         return response.resource;
                     }
                 }
-            },
-            get: {
-                transformResponse: function(data) {
-                    data = JSON.parse(data);
-                    var returnData = null;
-                    angular.forEach(data, function(prop, key){
-                        if(!prop._id)
-                            prop._id = key;
-                        returnData = prop;
-                    });
-                    return returnData;
-                }
             }
         })
-    }
+    }]);
 
-    ///////////////
-
-    function saveUpdateTransformRes(data) {
-        return {
-            _id: data
-        };
-    }
-
-    function jsonEscape(str)  {
+    function jsonEscape(str) {
         return str.replace(/\n/g, "\\\\n").replace(/\r/g, "\\\\r").replace(/\t/g, "\\\\t");
     }
 })();
