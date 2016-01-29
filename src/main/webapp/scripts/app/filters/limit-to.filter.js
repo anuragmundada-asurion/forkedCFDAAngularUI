b299da4e159799b33242f6c7828db3e540501ca0
RobertSmith-REISYS <robert.smith@reisys.com>
2016-01-29 00:17:46 -0500

    Add unit tests for Filter: limitTo

--- src/main/webapp/scripts/app/filters/limit-to.filter.js
+++ src/main/webapp/scripts/app/filters/limit-to.filter.js
@@ -7,27 +7,34 @@
 
     function limitToFilter() {
         return function(input, limit, begin) {
-            if (Math.abs(Number(limit)) === Infinity) {
-                limit = Number(limit);
-            } else {
-                limit = parseInt(limit);
-            }
-            if (isNaN(limit)) return input;
+			if (Math.abs(Number(limit)) === Infinity) {
+			  limit = Number(limit);
+			} else {
+			  limit = toInt(limit);
+			}
+			if (isNaN(limit)) return input;
 
-            if (angular.isNumber(input)) input = input.toString();
-            if (!angular.isArray(input) && !isString(input)) return input;
+			if (angular.isNumber(input)) input = input.toString();
+			if (!angular.isArray(input) && !angular.isString(input)) return input;
 
-            begin = (!begin || isNaN(begin)) ? 0 : parseInt(begin);
-            begin = (begin < 0 && begin >= -input.length) ? input.length + begin : begin;
-            if (limit >= 0) {
-                return input.slice(begin, begin + limit);
-            } else {
-                if (begin === 0) {
-                    return input.slice(limit, input.length);
-                } else {
-                    return input.slice(Math.max(0, begin + limit), begin);
-                }
-            }
+			begin = (!begin || isNaN(begin)) ? 0 : toInt(begin);
+			begin = (begin < 0) ? Math.max(0, input.length + begin) : begin;
+
+			if (limit >= 0) {
+			  return input.slice(begin, begin + limit);
+			} else {
+			  if (begin === 0) {
+				return input.slice(limit, input.length);
+			  } else {
+				return input.slice(Math.max(0, begin + limit), begin);
+			  }
+			}
         };
+		
+		///////////
+		
+		function toInt(str) {
+		  return parseInt(str, 10);
+		}
     }
 }();
