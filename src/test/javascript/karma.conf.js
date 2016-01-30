9716e75a46572432548f2411b3f9713e6b0f8d30
RobertSmith-REISYS <robert.smith@reisys.com>
2016-01-29 20:41:50 -0500

    Add ngHtml2JsPreprocessor for karma to pull templates

--- src/test/javascript/karma.conf.js
+++ src/test/javascript/karma.conf.js
@@ -12,11 +12,29 @@ module.exports = function(config) {
         // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
         frameworks: ['jasmine'],
 
-
         // list of files / patterns to load in the browser
         files: [
             // bower:js
+            'main/webapp/bower_components/jquery/dist/jquery.js',
+            'main/webapp/bower_components/angular/angular.js',
+            'main/webapp/bower_components/angular-resource/angular-resource.js',
+            'main/webapp/bower_components/angular-touch/angular-touch.js',
+            'main/webapp/bower_components/angular-animate/angular-animate.js',
+            'main/webapp/bower_components/angular-sanitize/angular-sanitize.js',
+            'main/webapp/bower_components/ui-router/release/angular-ui-router.js',
+            'main/webapp/bower_components/angular-smart-table/dist/smart-table.js',
+            'main/webapp/bower_components/angular-scroll/angular-scroll.js',
+            'main/webapp/bower_components/lodash/dist/lodash.compat.js',
+            'main/webapp/bower_components/angular-wizard/dist/angular-wizard.min.js',
+            'main/webapp/bower_components/ui-select/dist/select.js',
+            'main/webapp/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+            'main/webapp/bower_components/xdomain/dist/xdomain.js',
+            'main/webapp/bower_components/matchmedia/matchMedia.js',
+            'main/webapp/bower_components/ngSticky/lib/sticky.js',
+            'main/webapp/bower_components/angular-vs-repeat/src/angular-vs-repeat.js',
+            'main/webapp/bower_components/angular-mocks/angular-mocks.js',
             // endbower
+            'main/webapp/scripts/app/**/*.html',
             'main/webapp/scripts/app/app.module.js',
             'main/webapp/scripts/app/app.config.js',
             'main/webapp/scripts/app/constants.js',
@@ -33,9 +51,15 @@ module.exports = function(config) {
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
-            "main/webapp/scripts/app/**/*.js": "coverage"
+            "main/webapp/scripts/app/**/*.js": "coverage",
+            'main/webapp/scripts/app/**/*.html' : "ng-html2js"
         },
 
+        ngHtml2JsPreprocessor: {
+            stripPrefix: 'main/webapp/scripts/app/',
+            prependPrefix: 'partials/',
+            moduleName: 'templates'
+        },
 
         // test results reporter to use
         // possible values: 'dots', 'progress'
