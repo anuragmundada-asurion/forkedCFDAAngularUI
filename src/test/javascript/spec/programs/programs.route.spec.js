6d4c2e521006668a5b0e912c0df06e473af61397
RobertSmith-REISYS <robert.smith@reisys.com>
2016-01-30 06:31:59 -0500

    Add partial unit test for Program routes

--- src/test/javascript/spec/programs/programs.route.spec.js
+++ src/test/javascript/spec/programs/programs.route.spec.js
@@ -1,38 +1,60 @@
 'use strict';
 
-describe("Unit Tests for HTTP Code Routes", function () {
-    var $window, $state, $rootScope, $location, $templateCache, $httpBackend;
+describe("Unit Tests for Program Routes", function () {
+    var $state,
+        $stateParams,
+        $rootScope,
+        $location,
+        $injector,
+        $timeout,
+        $httpBackend,
+        appConstants,
+        coreChoices;
 
-    function mockTemplate(templateRoute, tpl) {
-        $templateCache.put(templateRoute, tpl || templateRoute);
-        $httpBackend.expectGET(templateRoute);
-    }
-
-    function goTo(url) {
-        $location.url(url);
-        $rootScope.$digest();
+    function goFrom(url) {
+        return {toState: function (state, params) {
+            $location.replace().url(url);
+            $state.go(state, params);
+            $rootScope.$digest();
+        }};
     }
 
     beforeEach(function() {
+        coreChoices = '{"yes_no":{"yes":{"element_id":"yes","description":"Yes","value":"Yes","code":"yes","elements":null},"no":{"element_id":"no","description":"No","value":"No","code":"no","elements":null}},"yes_na":{"yes":{"element_id":"yes","description":null,"value":"Yes","code":"yes","elements":null},"na":{"element_id":"na","description":"Not Applicable","value":"Not Applicable","code":"na","elements":null}},"yes_no_na":{"yes":{"element_id":"yes","description":"Yes","value":"Yes","code":"yes","elements":null},"no":{"element_id":"no","description":"No","value":"No","code":"no","elements":null},"na":{"element_id":"na","description":"Not Applicable","value":"Not Applicable","code":"na","elements":null}},"authorization_type":{"act":{"element_id":"act","description":null,"value":"Act","code":"act","elements":null},"eo":{"element_id":"eo","description":null,"value":"Executive Order","code":"eo","elements":null},"publiclaw":{"element_id":"publiclaw","description":null,"value":"Public Law","code":"publiclaw","elements":null},"statute":{"element_id":"statute","description":null,"value":"Statute","code":"statute","elements":null},"usc":{"element_id":"usc","description":null,"value":"USC","code":"usc","elements":null}}}';
         module('app');
-        inject(function(_$window_, _$state_, _$rootScope_, _$location_, _$templateCache_, _$httpBackend_){
-            $window = _$window_;
+        module(function($provide) {
+            $provide.value("Dictionary", {
+                toDropdown: function() {
+                    return {
+                        $promise: coreChoices
+                    }
+                }
+            });
+        });
+        module('templates');
+        inject(function(_$state_, _$stateParams_, _$rootScope_, _$location_, _$injector_, _$timeout_, _$httpBackend_, _appConstants_){
             $state = _$state_;
+            $stateParams = _$stateParams_;
             $rootScope = _$rootScope_;
             $location = _$location_;
-            $templateCache = _$templateCache_;
+            $injector = _$injector_;
+            $timeout = _$timeout_;
             $httpBackend = _$httpBackend_;
-
-            $httpBackend.whenGET('/environment/api').respond('Test API');
-
-            $httpBackend.expectGET('/environment/api');
-
-            mockTemplate('partials/httpcode/404.tpl.html');
+            appConstants = _appConstants_;
         });
     });
 
-    it('should go to 404 state if route is undefined', function(){
-        goTo('/NoPath');
-        expect($state.is('404')).toBe(true);
+    it("should respond to the 'addProgram' url", function() {
+        expect($state.href('addProgram', { section: 'info' })).toEqual('#/programs/add/info');
+    });
+
+    it("should go to 'addProgram' state with a new program object and core dictionary choices", function(){
+
+        goFrom('/#/').toState('addProgram', { section: 'info' });
+        expect($state.is('addProgram')).toBe(true);
+        expect($stateParams.section).toBe("info");
+        console.log($state.current.resolve.coreChoices);
+        expect(($injector.invoke($state.current.resolve.program))).toBeDefined();
+        expect($injector.invoke($state.current.resolve.coreChoices)).toEqual(coreChoices);
     });
 });
