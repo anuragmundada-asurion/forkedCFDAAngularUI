7eb1716d97dee38ebd32eb92592f8335f11c8578
Hieu Trang <hieu.trang@reisystems.com>
2016-02-04 16:05:14 -0500

    Updated tests for proxy service

--- src/test/javascript/spec/services/dictionary.services.spec.js
+++ src/test/javascript/spec/services/dictionary.services.spec.js
@@ -5,11 +5,6 @@ describe("Unit Tests for Dictionary Service", function () {
     beforeEach(function() {
         module('app');
 
-        var env = {'pub.api.programs': '/api', 'setApi': function(){}};
-        module(function($provide) {
-            $provide.value('env', env);
-        });
-
         inject(function(_$httpBackend_, _Dictionary_){
             $httpBackend = _$httpBackend_;
             dictionarySvc = _Dictionary_;
@@ -49,7 +44,7 @@ describe("Unit Tests for Dictionary Service", function () {
         });
         it('should make a GET call to retrieve a multiple regular dictionary in a tree format', function() {
             $httpBackend
-                .whenGET('http://gsaiae-cfda-program-uat01.reisys.com/api/v1/dictionaries?ids=functional_codes,assistance_type')
+                .whenGET('/api/dictionaries?ids=functional_codes,assistance_type')
                 .respond('[{"id":"functional_codes","elements":[{"element_id":"0001","description":null,"value":"AGRICULTURAL","code":null,"elements":[{"element_id":"0001001","description":null,"value":"Resource Conservation and Development","code":"AK","elements":null}]},{"element_id":"0002","description":null,"value":"BUSINESS AND COMMERCE","code":null,"elements":[{"element_id":"0002001","description":null,"value":"Small Business","code":"BK","elements":null}]}]},{"id":"assistance_type","elements":[{"element_id":"0001","description":"Formula Grants - Allocations of money to States or their subdivisions in accordance with a distribution formula prescribed by law or administrative regulation, for activities of a continuing nature not confined to a specific project.","value":"FORMULA GRANTS","code":"A","elements":[{"element_id":"0001001","description":null,"value":"Formula Grants","code":"A","elements":null}]},{"element_id":"0003","description":"Project Grants - The funding, for fixed or known periods, of specific projects or the delivery of specific services or products without liability for damages for failure to perform. Project grants include fellowships, scholarships, research grants, training grants, traineeships, experimental and demonstration grants, evaluation grants, planning grants, technical assistance grants, survey grants, construction grants, and unsolicited contractual agreements.","value":"PROJECT GRANTS","code":"B","elements":[{"element_id":"0003001","description":null,"value":"Cooperative Agreements","code":"B","elements":null}]}]}]');
             var dictionaries = dictionarySvc.query({ ids: ['functional_codes','assistance_type'].join(',') });
             $httpBackend.flush();
