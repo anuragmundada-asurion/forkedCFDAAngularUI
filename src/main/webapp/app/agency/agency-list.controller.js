c8447afc2cdde18f1bb72f789a349c0fb0f21f79
ltanguay <lsingh@techflow.com>
2016-03-04 13:30:43 -0500

    Updated Regional Agency Offices

--- src/main/webapp/app/agency/agency-list.controller.js
+++ src/main/webapp/app/agency/agency-list.controller.js
@@ -2,11 +2,14 @@
     "use strict";
 
      var myApp = angular.module('app');
-     myApp.controller('AgencyListController', ['$scope', '$state', '$stateParams', 'appConstants', 'ApiService',
-            function($scope, $state, $stateParams, appConstants, ApiService) {
+     myApp.controller('AgencyListController', ['$scope', '$state', '$stateParams', 'appConstants', 'ApiService', 'Dictionary',
+            function($scope, $state, $stateParams, appConstants, ApiService, Dictionary) {
                  $scope.itemsByPage = appConstants.DEFAULT_PAGE_ITEM_NUMBER;
                  $scope.itemsByPageNumbers= appConstants.PAGE_ITEM_NUMBERS;
-
+                var DICTIONARIES = [
+                                     'states',
+                                     'regional_office_division'
+                                 ];
 
                  /**
                   * Function loading agencies
@@ -49,19 +52,7 @@
                      $scope.promise = ApiService.call(oApiParam).then(
                          function(data) {
                              var agencies = [];
-                             //cleanup and adjust strutre data
-                            /* if($scope.programStatus === 'requests'){
-                                 programs = data.results;
-                             } else {*/
-                                 angular.forEach(data.results, function (item) {
-                                     angular.forEach(item, function (prop, key) {
-                                         if (!prop._id)
-                                             prop._id = key;
-                                         agencies.push(prop);
-                                     });
-                                 });
-                           /*  }*/
-
+                             agencies = data.results;
                              $scope.agencies = agencies;
                              $scope.isLoading = false;
 
