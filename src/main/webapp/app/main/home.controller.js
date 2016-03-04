e369988645d6f8fa491ef01ed30c800c03468c58
jahangir-iqbal-rei <jahangir.iqbal@reisystems.com>
2016-03-04 11:09:44 -0500

    program listing dummy data

--- src/main/webapp/app/main/home.controller.js
+++ src/main/webapp/app/main/home.controller.js
@@ -52,29 +52,82 @@
                 });
 
 
+                //gonna get data for 2014
+                console.log("A. get data for 2014");
+
+                $scope.year2 = new moment().subtract(2, 'year').format('YYYY');
+                console.log('year2 is : ' + $scope.year2);
+
+                var apiParams2 = {
+                    apiName: 'programCount',
+                    apiSuffix: '/' + $scope.year2,
+                    oParams: {},
+                    oData: {},
+                    method: 'GET'
+                };
+
+                console.log('apiParams2:');
+                console.log(apiParams2);
+                $scope.programCount2 = {
+                    'new': 212,
+                    'archived': 112,
+                    'updated': 423
+                };
+                console.log('about to call api service to make a call to this app\'s backend');
+                ApiService.call(apiParams2).then(function (data) {
+                    console.log('after the api call was made, the returned data: ' + data);
+                    $scope.programCount2 = {
+                        'new': d3.format($scope.formatNumber(parseInt(data.new)))(data.new),
+                        'archived': d3.format($scope.formatNumber(parseInt(data.archived)))(data.archived),
+                        'updated': d3.format($scope.formatNumber(parseInt(data.updated)))(data.updated)
+                    };
+                    console.log("program count:");
+                    console.log($scope.programCount2);
+                });
+
+                console.log("Z. get data for 2014");
+
+                console.log($scope.programCount2);
+
                 //make chart
                 $scope.chart = c3.generate({
                     bindto: document.getElementById('listingsChart'),
                     data: {
-                        json: [{
-                            name: 'Individual',
-                            total: 400
-                        }, {
-                            name: 'Local',
-                            total: 400
-                        }, {
-                            name: 'Nonprofit',
-                            total: 500
-                        }, {
-                            name: 'State',
-                            total: 5000
-                        }, {
-                            name: 'U.S. Territories',
-                            total: 100
-                        }, {
-                            name: 'Federally Recognized Indian Tribal Organizations',
-                            total: 800
-                        }
+                        json: [
+                            {
+                                name: 'New',
+                                total: $scope.programCount2.new
+                            }, {
+                                name: 'Archived',
+                                total: $scope.programCount2.archived
+                            }, {
+                                name: 'Updated',
+                                total: $scope.programCount2.updated
+                            }
+
+
+
+
+
+                        //    {
+                        //    name: 'Individual',
+                        //    total: 400
+                        //}, {
+                        //    name: 'Local',
+                        //    total: 400
+                        //}, {
+                        //    name: 'Nonprofit',
+                        //    total: 500
+                        //}, {
+                        //    name: 'State',
+                        //    total: 5000
+                        //}, {
+                        //    name: 'U.S. Territories',
+                        //    total: 100
+                        //}, {
+                        //    name: 'Federally Recognized Indian Tribal Organizations',
+                        //    total: 800
+                        //}
                         ],
                         keys: {
                             x: 'name', // it's possible to specify 'x' when category axis
--- src/test/javascript/karma.conf.js
+++ src/test/javascript/karma.conf.js
@@ -16,6 +16,8 @@ module.exports = function(config) {
         files: [
             // bower:js
             'main/webapp/bower_components/jquery/dist/jquery.min.js',
+            'main/webapp/bower_components/es5-shim/es5-shim.js',
+            'main/webapp/bower_components/es6-shim/es6-shim.js',
             'main/webapp/bower_components/angular/angular.js',
             'main/webapp/bower_components/angular-resource/angular-resource.js',
             'main/webapp/bower_components/angular-touch/angular-touch.js',
@@ -27,7 +29,7 @@ module.exports = function(config) {
             'main/webapp/bower_components/angular-wizard/dist/angular-wizard.min.js',
             'main/webapp/bower_components/ui-select/dist/select.js',
             'main/webapp/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
-            'main/webapp/bower_components/xdomain/dist/xdomain.js',
+            'main/webapp/bower_components/xdomain/dist/xdomain.min.js',
             'main/webapp/bower_components/ng-dialog/js/ngDialog.js',
             'main/webapp/bower_components/matchmedia/matchMedia.js',
             'main/webapp/bower_components/ngSticky/lib/sticky.js',
