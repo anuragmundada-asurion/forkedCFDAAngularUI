39391018e7c2a3874836aac7abea0c224cf7e2ee
jahangir-iqbal-rei <jahangir.iqbal@reisystems.com>
2016-03-15 12:41:49 -0400

    small changes

--- src/main/webapp/app/programs/view/viewProgram.controller.js
+++ src/main/webapp/app/programs/view/viewProgram.controller.js
@@ -6,18 +6,11 @@
     app.controller('ViewProgramCtrl', ['$state', '$scope', '$stateParams', 'appConstants', 'SearchFactory', 'ProgramFactory', 'Dictionary',
         function ($state, $scope, $stateParams, appConstants, SearchFactory, ProgramFactory, Dictionary) {
             console.log("hello world from viewProgramController... yay!");
-            $scope.program = {};
-
-            if ($state.current['name'] === 'viewProgram') {
-                ProgramFactory.get({id: $stateParams.id}).$promise.then(function (data) {
-                    $scope.program = data;
-                    console.log($scope.program);
-                });
-            } else {
-                console.log("state is NOT view program!!!");
-                console.log($state.current['name']);
-            }
-
+            ProgramFactory.get({id: $stateParams.id}).$promise.then(function(data) {
+                $scope.program = data;
+                $scope.programData = $scope.program.data;
+                console.log($scope.program);
+            });
         }
     ]);
 }();
