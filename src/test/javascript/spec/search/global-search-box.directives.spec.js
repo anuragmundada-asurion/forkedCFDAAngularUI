231bf791f49c9bb440f8b76fec03948495d47107
JihadMotii-REISys <jihad.motii@reisystems.com>
2016-03-16 18:36:06 -0400

    update unit test / Advanced search

--- src/test/javascript/spec/search/global-search-box.directives.spec.js
+++ src/test/javascript/spec/search/global-search-box.directives.spec.js
@@ -4,12 +4,20 @@ describe('Unit Tests for Global Search Box Directive', function() {
     var element;
     var $rootScope;
     var $compile;
+    var $httpBackend;
 
-    beforeEach(inject(function(_$rootScope_, _$compile_) {
-        element = '<global-search-box></global-search-box>';
-        $rootScope = _$rootScope_;
-        $compile = _$compile_;
-    }));
+    beforeEach(function() {
+        inject(function(_$rootScope_, _$compile_, _$httpBackend_) {
+            element = '<global-search-box></global-search-box>';
+            $rootScope = _$rootScope_;
+            $compile = _$compile_;
+            $httpBackend = _$httpBackend_;
+        });
+
+        $httpBackend
+            .whenGET(/\/api\/dictionaries(\?ids=[a-z_]+)*/i)
+            .respond({});
+    });
 
     it('should only allow elements', function() {
         var invalidDirective = '<div global-search-box="" class="ng-scope"></div>';
