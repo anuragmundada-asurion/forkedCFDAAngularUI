2021e187e5b8341e6b0fbb3cedfd0679e685b8da
jahangir-iqbal-rei <jahangir.iqbal@reisystems.com>
2016-03-16 14:49:51 -0400

    dictionary, assistance_type

--- src/main/webapp/app/programs/view/viewProgram.controller.js
+++ src/main/webapp/app/programs/view/viewProgram.controller.js
@@ -3,13 +3,29 @@
 
     var app = angular.module('app');
 
-    app.controller('ViewProgramCtrl', ['$state', '$scope', '$stateParams', 'appConstants', 'SearchFactory', 'ProgramFactory', 'Dictionary',
-        function ($state, $scope, $stateParams, appConstants, SearchFactory, ProgramFactory, Dictionary) {
-            console.log("hello world from viewProgramController... yay!");
+    app.controller('ViewProgramCtrl', ['$state', '$scope', '$stateParams',  '$filter', '$parse', 'appConstants', 'SearchFactory', 'ProgramFactory', 'Dictionary',
+        function ($state, $scope, $stateParams,  $filter, $parse, appConstants, SearchFactory, ProgramFactory, Dictionary) {
             ProgramFactory.get({id: $stateParams.id}).$promise.then(function(data) {
                 $scope.programData = data;
                 console.log($scope.programData);
             });
+
+            Dictionary.query({ ids: ['assistance_type'] }, function(data) {
+                //Assistance Types
+                $scope.aAssistanceType = data['assistance_type'];
+            });
+
+            $scope.traverseTree = function(value) {
+                var selected = $filter('traverseTree')([value], $scope.aAssistanceType, {
+                    branches: {
+                        X: {
+                            keyProperty: $parse('element_id'),
+                            childrenProperty: $parse('elements')
+                        }
+                    }
+                })[0];
+                return selected ? selected.$original : null;
+            }
         }
     ]);
 }();
