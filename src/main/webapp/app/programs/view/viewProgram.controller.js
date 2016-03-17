b5001350a17daad1a1d3a8765a3766b9bdb3e6f2
jahangir-iqbal-rei <jahangir.iqbal@reisystems.com>
2016-03-17 13:24:42 -0400

    applicant types, showing values now, instead of ids

--- src/main/webapp/app/programs/view/viewProgram.controller.js
+++ src/main/webapp/app/programs/view/viewProgram.controller.js
@@ -10,13 +10,17 @@
                 //console.log($scope.programData);
             });
 
-            Dictionary.query({ ids: ['assistance_type'] }, function(data) {
+            Dictionary.query({ ids: ['assistance_type', 'applicant_types'] }, function(data) {
                 //Assistance Types
                 $scope.aAssistanceType = data['assistance_type'];
+                $scope.aApplicantType = data['applicant_types'];
+                console.log(data);
+                $scope.allTypes = $scope.aAssistanceType.concat($scope.aApplicantType);
+
             });
 
             $scope.traverseTree = function(value) {
-                var selected = $filter('traverseTree')([value], $scope.aAssistanceType, {
+                var selected = $filter('traverseTree')([value], $scope.allTypes, {
                     branches: {
                         X: {
                             keyProperty: $parse('element_id'),
@@ -24,8 +28,15 @@
                         }
                     }
                 })[0];
+                if(selected){
+                    console.log("returnging this: " + selected.$original);
+                }else{
+                    console.log("selected was null");
+                }
                 return selected ? selected.$original : null;
-            }
+            };
+
+
         }
     ]);
 }();
