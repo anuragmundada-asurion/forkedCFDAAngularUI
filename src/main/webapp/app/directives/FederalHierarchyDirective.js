616cb927175d322447bdd9ab4fba5dc544a54525
jahangir-iqbal-rei <jahangir.iqbal@reisystems.com>
2016-03-23 20:56:26 -0400

    federal hierarchy directive fix

--- src/main/webapp/app/directives/FederalHierarchyDirective.js
+++ src/main/webapp/app/directives/FederalHierarchyDirective.js
@@ -5,18 +5,22 @@
 
     myApp.directive('federalHierarchyLabel', ['FederalHierarchyService', function(FederalHierarchyService) {
         return {
-            restrict: 'EA',
+
             scope: {
                 "organizationId": "@"
             },
             link: function(scope, element, attributes) {
-                //Call FederalHierarchy API and get Label of the organization
-                FederalHierarchyService.getFederalHierarchyById(scope.organizationId, function(oOrganization){
-                    if(oOrganization && oOrganization.hasOwnProperty('name')){
-                        scope.organizationName = oOrganization.name;
+                attributes.$observe('organizationId', function(value){
+                    if(value) {
+                        //Call FederalHierarchy API and get Label of the organization
+                        FederalHierarchyService.getFederalHierarchyById(scope.organizationId, function (oOrganization) {
+                            if (oOrganization && oOrganization.hasOwnProperty('name')) {
+                                scope.organizationName = oOrganization.name;
+                            }
+                        }, function (error) {
+                            scope.organizationName = "An error has occurred, Please try again !";
+                        });
                     }
-                }, function(error){
-                    scope.organizationName = "An error has occurred, Please try again !";
                 });
             },
             template: '<img ng-show="!organizationName" style="max-width: 10%;" src="/img/img_cfda/loading.svg" />{{ organizationName }}'
