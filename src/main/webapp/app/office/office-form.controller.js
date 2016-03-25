cdfc9a5df1cc2c46ab0590c002c8bc056590059b
JihadMotii-REISys <jihad.motii@reisystems.com>
2016-03-25 15:05:29 -0400

    Regional office -> delete

--- src/main/webapp/app/office/office-form.controller.js
+++ src/main/webapp/app/office/office-form.controller.js
@@ -113,37 +113,39 @@
              * @returns void
              */
             $scope.deleteRegionalOffice = function(){
-                $scope.oRegionalOffice.$delete({id: $scope.oRegionalOffice.id}).then(function(data){
-                    ngDialog.open({
-                        template: '<div class="usa-alert usa-alert-success" role="alert">'+
-                                    '<div class="usa-alert-body">'+
-                                      '<p class="usa-alert-text">This Regional agency office has been successfully deleted.</p>'+
-                                    '</div>'+
-                                  '</div>',
-                        plain: true,
-                        closeByEscape: true,
-                        showClose: true
-                    });
+                if(confirm("Are you sure you want to delete this Regional agency office?")) {
+                    $scope.oRegionalOffice.$delete({id: $scope.oRegionalOffice.id}).then(function(data){
+                        ngDialog.open({
+                            template: '<div class="usa-alert usa-alert-success" role="alert">'+
+                                        '<div class="usa-alert-body">'+
+                                          '<p class="usa-alert-text">This Regional agency office has been successfully deleted.</p>'+
+                                        '</div>'+
+                                      '</div>',
+                            plain: true,
+                            closeByEscape: true,
+                            showClose: true
+                        });
 
-                    //go to list page after 2 seconds
-                    $timeout(function() {
-                        ngDialog.closeAll();
-                        $state.go('regionalOfficeList');
-                    }, 3000);
-                }, 
-                function(error){
-                    ngDialog.open({
-                        template: '<div class="usa-alert usa-alert-error" role="alert">'+
-                                    '<div class="usa-alert-body">'+
-                                      '<h3 class="usa-alert-heading">Error Status</h3>'+
-                                      '<p class="usa-alert-text">An error has occurred, please try again!</p>'+
-                                    '</div>'+
-                                  '</div>',
-                        plain: true,
-                        closeByEscape: true,
-                        showClose: true
+                        //go to list page after 2 seconds
+                        $timeout(function() {
+                            ngDialog.closeAll();
+                            $state.go('regionalOfficeList');
+                        }, 3000);
+                    }, 
+                    function(error){
+                        ngDialog.open({
+                            template: '<div class="usa-alert usa-alert-error" role="alert">'+
+                                        '<div class="usa-alert-body">'+
+                                          '<h3 class="usa-alert-heading">Error Status</h3>'+
+                                          '<p class="usa-alert-text">An error has occurred, please try again!</p>'+
+                                        '</div>'+
+                                      '</div>',
+                            plain: true,
+                            closeByEscape: true,
+                            showClose: true
+                        });
                     });
-                });
+                }
             };
     }]);
 })();
--- src/main/webapp/app/office/office-view.controller.js
+++ src/main/webapp/app/office/office-view.controller.js
@@ -3,11 +3,62 @@
 
     var myApp = angular.module('app');
 
-    myApp.controller('RegionalOfficeViewCtrl', ['$scope', '$stateParams', 'RegionalOfficeFactory',
-        function($scope, $stateParams, RegionalOfficeFactory) {
+    myApp.controller('RegionalOfficeViewCtrl', ['$scope', '$state', '$stateParams', '$timeout', 'RegionalOfficeFactory', 'Dictionary', 'ngDialog',
+        function($scope, $state, $stateParams, $timeout, RegionalOfficeFactory, Dictionary, ngDialog) {
+            
             RegionalOfficeFactory.get({id: $stateParams.id}).$promise.then(function(data){
                 $scope.oRegionalOffice = data;
+
+                var aDictionay = [ 'regional_office_division', 'states' ];
+                Dictionary.toDropdown({ ids: aDictionay.join(',') }).$promise.then(function(data){
+                    angular.forEach(data.regional_office_division, function(oItem){
+                        if(oItem.element_id === $scope.oRegionalOffice.division) {
+                            $scope.oDivision = oItem;
+                            return;
+                        }
+                    });
+                });
             });
+
+            /**
+             * delete regional office
+             * @returns void
+             */
+            $scope.deleteRegionalOffice = function(){
+                if(confirm("Are you sure you want to delete this Regional agency office?")) {
+                    $scope.oRegionalOffice.$delete({id: $scope.oRegionalOffice.id}).then(function(data){
+                        ngDialog.open({
+                            template: '<div class="usa-alert usa-alert-success" role="alert">'+
+                                        '<div class="usa-alert-body">'+
+                                          '<p class="usa-alert-text">This Regional agency office has been successfully deleted.</p>'+
+                                        '</div>'+
+                                      '</div>',
+                            plain: true,
+                            closeByEscape: true,
+                            showClose: true
+                        });
+
+                        //go to list page after 2 seconds
+                        $timeout(function() {
+                            ngDialog.closeAll();
+                            $state.go('regionalOfficeList');
+                        }, 3000);
+                    }, 
+                    function(error){
+                        ngDialog.open({
+                            template: '<div class="usa-alert usa-alert-error" role="alert">'+
+                                        '<div class="usa-alert-body">'+
+                                          '<h3 class="usa-alert-heading">Error Status</h3>'+
+                                          '<p class="usa-alert-text">An error has occurred, please try again!</p>'+
+                                        '</div>'+
+                                      '</div>',
+                            plain: true,
+                            closeByEscape: true,
+                            showClose: true
+                        });
+                    });
+                }
+            };
         }
     ]);
 }();
