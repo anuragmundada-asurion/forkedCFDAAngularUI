28fd29ec7a20f303e3c143d385e2ee812dc7ba31
Hieu Trang <hieu.trang@reisystems.com>
2016-03-31 18:58:29 -0400

    Disable title for revisions

---
+++ src/main/webapp/app/programs/program.model.js
@@ -0,0 +1,38 @@
+!function() {
+    'use strict';
+
+    var myApp = angular.module('app');
+
+    myApp.factory('Program', [function() {
+        function Program(program) {
+            //  TODO Parse incoming obj
+            angular.extend(this, program);
+        }
+
+        Program.prototype = {
+            getProgramNumber: function() {
+                return this.programNumber;
+            },
+            getStatus: function(){
+                return this.status ? this.status : '';
+            },
+            isArchived: function() {
+                return this.archived;
+            },
+            isDraft: function() {
+                return this.getStatus().toLowerCase() === 'draft' && !this.isArchived();
+            },
+            isRejected: function() {
+                return this.getStatus().toLowerCase() === 'rejected' && !this.isArchived();
+            },
+            isRevision: function() {
+                return this.getProgramNumber() && this.isDraft();
+            },
+            isTitleEditable: function() {
+                return (this.isDraft() || this.isRejected()) && !this.isRevision();
+            }
+        };
+
+        return Program;
+    }]);
+}();
