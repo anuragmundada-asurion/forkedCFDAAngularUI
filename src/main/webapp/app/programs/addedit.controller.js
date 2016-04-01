4784d820848f93e3dcb928c95a083c400e4990a2
jahangir-iqbal-rei <jahangir.iqbal@reisystems.com>
2016-03-31 21:10:07 -0400

    financial section validations,

--- src/main/webapp/app/programs/addedit.controller.js
+++ src/main/webapp/app/programs/addedit.controller.js
@@ -384,6 +384,7 @@
                 }
 
                 function onSectionChange(prevSectionKey) {
+                    console.log(vm.program);
                     save();
                     revealValidations(prevSectionKey);
                 }
