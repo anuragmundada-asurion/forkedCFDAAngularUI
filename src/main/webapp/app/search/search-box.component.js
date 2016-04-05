d4d599d4f5407c7bd97f320b86ec8be0ee38d27f
Carlos Valle <carlos.m.valle@gmail.com>
2016-04-05 13:13:26 -0400

    Search box component markup

---
+++ src/main/webapp/app/search/search-box.component.js
@@ -0,0 +1,14 @@
+!function() {
+    'use strict';
+
+    angular.module('app').directive('searchBox', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'search/search-box.tpl.html',
+            link: function(scope, element, attr, controllers){
+                console.log("enable accordion");
+                $('.ui.accordion').accordion();
+            }
+        };
+    });
+}();
