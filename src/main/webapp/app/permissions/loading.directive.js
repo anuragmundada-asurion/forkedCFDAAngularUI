3a89e7094229dcc55fc9adbe6e97b6d4d5859d59
Hieu Trang <hieu.trang@reisystems.com>
2016-04-07 17:26:06 -0400

    Styles for loading modal on home page

--- src/main/webapp/app/permissions/loading.directive.js
+++ src/main/webapp/app/permissions/loading.directive.js
@@ -7,7 +7,7 @@
         return {
             restrict: 'E',
             replace: true,
-            template: '<div class="loading-modal"><div class="loading-icon"></div></div>',
+            template: '<div style="width: 100%;height: 100%;z-index: 10000;top: 0;left: 0;opacity: .75;position: fixed;background: black center;"><div style="background: url(\'/img/img_cfda/loading_white.svg\') no-repeat center;width: 100%;height: 100%;"></div></div>',
             link: function(scope, element) {
                 scope.$watch(
                     function() {
