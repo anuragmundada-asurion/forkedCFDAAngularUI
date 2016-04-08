7272f983456ead9a57f54761c33c7c68eb5e8d5c
JihadMotii-REISys <jihad.motii@reisystems.com>
2016-04-07 23:08:56 -0400

    CFDA-454: implementing UI federal hierarchy

--- src/main/webapp/app/directives/FederalHierarchyDirective.js
+++ src/main/webapp/app/directives/FederalHierarchyDirective.js
@@ -34,13 +34,11 @@
             scope: {
                 "organizationId": "=" // ngModel var passed by reference (two-way) 
             },
-            controller: ['$scope', function($scope) {
+            controller: ['$scope', 'ROLES', function($scope, ROLES) {
                 //store program organization id if the program has it otherwise store user's organization id
                 if(typeof $scope.programOrganizationId === 'undefined' ) {
                     $scope.programOrganizationId = (typeof $scope.organizationId === 'undefined' || $scope.organizationId === '' || $scope.organizationId === null) ? UserService.getUserOrgId() : $scope.organizationId;
                 }
-
-                console.log('Original orgID: '+$scope.programOrganizationId)
 
                 /**
                  * Function to set the organizationId scope from dropdowns (Department/Agency/Office)
@@ -48,12 +46,11 @@
                  * @returns void
                  */
                 $scope.setOrganizationId = function(type) {
-                    console.log('Before: '+ $scope.organizationId);
                     switch(type){
                         case 'department':
                             if(typeof $scope.selectedDeptId !== 'undefined' && $scope.selectedDeptId !== '' 
-                                    && $scope.selectedDeptId !== null && $scope.selectedDeptId.hasOwnProperty('elementId')) {
-                                $scope.organizationId = $scope.selectedDeptId.elementId;
+                                    && $scope.selectedDeptId !== null) {
+                                $scope.organizationId = $scope.selectedDeptId;
                             } else { //if department is not selected then set user's organization id
                                 $scope.organizationId = $scope.programOrganizationId;
                             }
@@ -64,22 +61,25 @@
                             $scope.dictionary.aOffice = [];
                             $scope.selectedOfficeId = null;
 
-                            //get agencies of the selected department
-                            $scope.initDictionaries($scope.organizationId, true, true, function (oData) {
-                                if(oData.type === 'DEPARTMENT') {
-                                    //initialize Department "Label" and Agency dropdown
-                                    $scope.dictionary.aAgency = oData.hierarchy;
-                                }
-                            });
+                            if(typeof $scope.selectedDeptId !== 'undefined' && $scope.selectedDeptId !== '' 
+                                    && $scope.selectedDeptId !== null) {
+                                //get agencies of the selected department
+                                $scope.initDictionaries($scope.organizationId, true, true, function (oData) {
+                                    if(oData.type === 'DEPARTMENT') {
+                                        //initialize Department "Label" and Agency dropdown
+                                        $scope.dictionary.aAgency = oData.hierarchy;
+                                    }
+                                });
+                            }
                             break;
                         case 'agency':
                             if(typeof $scope.selectedAgencyId !== 'undefined' && $scope.selectedAgencyId !== '' 
-                                    && $scope.selectedAgencyId !== null && $scope.selectedAgencyId.hasOwnProperty('elementId')) {
-                                $scope.organizationId = $scope.selectedAgencyId.elementId;
+                                    && $scope.selectedAgencyId !== null) {
+                                $scope.organizationId = $scope.selectedAgencyId;
                             } else { //if agency is not selected then set department
                                 //if user is a root then set department from dropdown
                                 if(AuthorizationService.authorizeByRole([ROLES.SUPER_USER])) {
-                                    $scope.organizationId = $scope.selectedDeptId.elementId;
+                                    $scope.organizationId = $scope.selectedDeptId;
                                 } else if(AuthorizationService.authorizeByRole([ROLES.AGENCY_COORDINATOR])) { //if user is a agency coord then set department from user's
                                     $scope.organizationId = $scope.programOrganizationId;
                                 }
@@ -92,22 +92,20 @@
                             //get offices of the selected agency
                             $scope.initDictionaries($scope.organizationId, false, true, function (oData) {
                                 if(oData.type === 'AGENCY') {
-                                    //initialize Department "Label" and Agency dropdown
+                                    //initialize Department "Label" and Office dropdown
                                     $scope.dictionary.aOffice = oData.hierarchy;
                                 }
                             });
                             break;
                         case 'office':
                             if(typeof $scope.selectedOfficeId !== 'undefined' && $scope.selectedOfficeId !== '' 
-                                && $scope.selectedOfficeId !== null && $scope.selectedOfficeId.hasOwnProperty('elementId')) {
-                                $scope.organizationId = $scope.selectedOfficeId.elementId;
+                                && $scope.selectedOfficeId !== null) {
+                                $scope.organizationId = $scope.selectedOfficeId;
                             } else { //if office is not selected then set agency
-                                $scope.organizationId = $scope.selectedAgencyId.elementId;
+                                $scope.organizationId = $scope.selectedAgencyId;
                             }
                             break;
                     }
-                    
-                    console.log('After: '+ $scope.organizationId);
                 };
 
                 /**
@@ -134,6 +132,62 @@
                         }
                     });
                 };
+
+                /**
+                 * initialize Department/Agency/Office dropdowns (selected values)
+                 * @param String userRole
+                 * @returns void
+                 */
+                $scope.initFederalHierarchyDropdowns = function(userRole) {
+                    //get current federal hierarchy by program organizationid or user organization id
+                    $scope.initDictionaries($scope.programOrganizationId, true, true, function (oData) {
+                        //if role is agency coordinator, assign the department
+                        if(userRole === ROLES.AGENCY_COORDINATOR.iamRoleId) {
+                            $scope.dictionary.aDepartment = [oData];
+                        }
+
+                        //if user is super admin -> set selected Department dropdown option
+                        if(userRole === ROLES.SUPER_USER.iamRoleId && oData.type === 'DEPARTMENT') {
+                            $scope.selectedDeptId = oData.elementId;
+                        }
+
+                        //if the program's organization id or user's organization id is different then department id -> fetch agencies/offices of that orgID
+                        if($scope.programOrganizationId !== oData.elementId) {
+                            //load agencies of the department Id
+                            $scope.initDictionaries(oData.elementId, true, true, function (oDepartment) {
+                                $scope.dictionary.aAgency = oDepartment.hierarchy;
+
+                                //looping through agencies and verify if the programOrganizationId is actually an agency or an office
+                                angular.forEach(oDepartment.hierarchy, function(oAgency) {
+                                    //check if $scope.programOrganizationId is an agency then set selected agency value and prepopulate offices of that agency
+                                    if(oAgency.elementId == $scope.programOrganizationId) {
+                                        $scope.selectedAgencyId = oAgency.elementId;
+                                        if(oAgency.hasOwnProperty('hierarchy')) {
+                                            $scope.dictionary.aOffice = oAgency.hierarchy;
+                                        }
+                                        return;
+                                    }
+
+                                    //check if this agency has offices, if yes, 
+                                    //loop through them and verify if $scope.programOrganizationId is an office if yes,
+                                    //assign selected values for agency and office and also populate offices
+                                    if(oAgency.hasOwnProperty('hierarchy')) {
+                                        angular.forEach(oAgency.hierarchy, function(oOffice){
+                                            if(oOffice.elementId == $scope.programOrganizationId) {
+                                                $scope.selectedAgencyId = oAgency.elementId;
+                                                $scope.selectedOfficeId = oOffice.elementId;
+                                                $scope.dictionary.aOffice = oAgency.hierarchy;
+                                                return;
+                                            }
+                                        });
+                                    }
+                                });
+                            });
+                        } else if($scope.programOrganizationId == oData.elementId) { //if the program's organization id or user's organization id is the same then prepopulate agencies
+                            $scope.dictionary.aAgency = oData.hierarchy;
+                        }
+                    });
+                };
             }],
             link: function(scope, element, attributes) {
                 scope.dictionary = {
@@ -144,7 +198,6 @@
                 scope.departmentLabel = '';
                 scope.error = '';
 
-                console.log('Original orgID: '+scope.programOrganizationId);
                 //Case if user is an AGENCY USER
                 if(AuthorizationService.authorizeByRole([ROLES.AGENCY_USER])) {
                     FederalHierarchyService.getFullLabelPathFederalHierarchyById(scope.programOrganizationId, true, false, function (organizationNames) {
@@ -155,27 +208,17 @@
                     });
                 } //Case if user is an AGENCY COORDINATOR
                 else if(AuthorizationService.authorizeByRole([ROLES.AGENCY_COORDINATOR])) {
-                    //get Department level of user's organizationId
-                    scope.initDictionaries(scope.programOrganizationId, true, true, function (oData) {
-                        if(oData.type === 'DEPARTMENT') {
-                            //initialize Department "Label" and Agency dropdown
-                            scope.dictionary = {
-                                aDepartment: [oData],
-                                aAgency: oData.hierarchy,
-                                aOffice: []
-                            };
-                        }
-                    });
+                    //initialize Department/Agency/Office dropdowns (selected values)
+                    scope.initFederalHierarchyDropdowns(ROLES.AGENCY_COORDINATOR.iamRoleId);
                 } //Case if user is ROOT
                 else if(AuthorizationService.authorizeByRole([ROLES.SUPER_USER])) {
                     //get Department level of user's organizationId
                     scope.initDictionaries('', true, false, function (oData) {
-                        //initialize Department "Label" and Agency dropdown
-                        scope.dictionary = {
-                            aDepartment: oData._embedded.hierarchy,
-                            aAgency: [],
-                            aOffice: []
-                        };
+                        //initialize Department
+                        scope.dictionary.aDepartment = oData._embedded.hierarchy;
+
+                        //initialize Department/Agency/Office dropdowns (selected values)
+                        scope.initFederalHierarchyDropdowns(ROLES.SUPER_USER.iamRoleId);
                     });
                 }
             },
@@ -187,21 +230,21 @@
                     "<div class='organization-container-form' has-role='["+JSON.stringify(ROLES.SUPER_USER)+","+ JSON.stringify(ROLES.AGENCY_COORDINATOR)+"]'>"+
                         "<div class='input-field usa-width-one-fourth'>"+
                             "<h3><label for='jqDepartmentFH'>Department</label></h3>"+
-                            "<select id='jqDepartmentFH' name='department' class='usa-form-medium' has-role='["+JSON.stringify(ROLES.SUPER_USER)+"]' ng-change='setOrganizationId(\"department\")' ng-model='selectedDeptId' ng-options='item.name for item in dictionary.aDepartment track by item.elementId' required>"+
+                            "<select id='jqDepartmentFH' name='department' class='usa-form-medium' has-role='["+JSON.stringify(ROLES.SUPER_USER)+"]' ng-change='setOrganizationId(\"department\")' ng-model='selectedDeptId' ng-options='item.elementId as item.name for item in dictionary.aDepartment' required>"+
                                 "<option value=''>Please select a Department</option>"+
                             "</select>"+
                             "<span class='departmen-label' has-role='["+JSON.stringify(ROLES.AGENCY_COORDINATOR)+"]'> {{ dictionary.aDepartment[0].name }} </span>"+
                         "</div>"+
                         "<div class='input-field usa-width-one-fourth'>"+
                             "<h3><label for='jqAgencyFH'>Agency</label></h3>"+
-                            "<select id='jqAgencyFH' name='agency' class='usa-form-medium' ng-change='setOrganizationId(\"agency\")' ng-model='selectedAgencyId' ng-options='item.name for item in dictionary.aAgency track by item.elementId'>"+
+                            "<select id='jqAgencyFH' name='agency' class='usa-form-medium' ng-change='setOrganizationId(\"agency\")' ng-model='selectedAgencyId' ng-options='item.elementId as item.name for item in dictionary.aAgency'>"+
                                 "<option value=''>Please select an Agency</option>"+
                             "</select>"+
 
                         "</div>"+
                         "<div class='input-field usa-width-one-fourth'>"+
                             "<h3><label for='jqOfficeFH'>Office</label></h3>"+
-                            "<select id='jqOfficeFH' name='office' class='usa-form-medium' ng-change='setOrganizationId(\"office\")' ng-model='selectedOfficeId' ng-options='item.name for item in dictionary.aOffice track by item.elementId'>"+
+                            "<select id='jqOfficeFH' name='office' class='usa-form-medium' ng-change='setOrganizationId(\"office\")' ng-model='selectedOfficeId' ng-options='item.elementId as item.name for item in dictionary.aOffice'>"+
                                 "<option value=''>Please select an Office</option>"+
                             "</select>"+
 
