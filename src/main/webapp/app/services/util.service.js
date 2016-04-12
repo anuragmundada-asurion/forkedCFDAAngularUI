7694abbc83b3e0ac49f95997e28ec21aa41cba02
Hieu Trang <hieu.trang@reisystems.com>
2016-04-12 16:56:04 -0400

    Refactored utility

--- src/main/webapp/app/services/util.service.js
+++ src/main/webapp/app/services/util.service.js
@@ -1,59 +1,54 @@
-(function() {
-    "use strict";
+!function() {
+    'use strict';
 
-    angular
-        .module('app')
-        .factory('util', utilSvc);
+    angular.isUndefinedOrNull = function(val) {
+        return angular.isUndefined(val) || val === null;
+    };
 
-    utilSvc.$inject = ['$window'];
+    var myApp = angular.module('app');
 
-    ////////////////
+    myApp.service('util', ['$window',
+        function($window) {
+            var crypto = $window.crypto || $window.msCrypto;
 
-    function utilSvc($window) {
-        var crypto = $window.crypto || $window.msCrypto,
-            fiscalYearStartMonth = new Date('10/1/2015').getMonth(),
-            currentId = 0;
+            this.fiscalYearStartMonth = new Date('10/1/2015').getMonth();
+            this.currentId = 0;
 
-        return {
-            generateUUID: crypto && crypto.getRandomValues ? generateUUID : generateUUIDPolyfill,
-            getFiscalYear: getFiscalYear,
-            nextId: nextId
-        };
-
-        ////////////////////
-
-        function generateUUID() {
-            var buf = new Uint16Array(8);
-            crypto.getRandomValues(buf);
-            return (S4(buf[0])+S4(buf[1])+"-"+S4(buf[2])+"-"+S4(buf[3])+"-"+S4(buf[4])+"-"+S4(buf[5])+S4(buf[6])+S4(buf[7]));
-        }
-
-        function S4(num) {
-            var ret = num.toString(16);
-            while(ret.length < 4){
-                ret = "0"+ret;
+            function S4(num) {
+                var ret = num.toString(16);
+                while(ret.length < 4){
+                    ret = "0"+ret;
+                }
+                return ret;
             }
-            return ret;
-        }
 
-        function generateUUIDPolyfill() {
-            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-                var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-                return v.toString(16);
-            });
-        }
+            if (crypto && crypto.getRandomValues) {
+                this.generateUUID = function() {
+                    var buf = new Uint16Array(8);
+                    crypto.getRandomValues(buf);
+                    return (S4(buf[0])+S4(buf[1])+"-"+S4(buf[2])+"-"+S4(buf[3])+"-"+S4(buf[4])+"-"+S4(buf[5])+S4(buf[6])+S4(buf[7]));
+                };
+            } else {
+                this.generateUUID = function() {
+                    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+                        return v.toString(16);
+                    });
+                };
+            }
 
-        function getFiscalYear(date) {
-            date = date || new Date();
-            var year = date.getFullYear(),
-                month = date.getMonth();
-            if(month >= fiscalYearStartMonth)
-                year++;
-            return year;
-        }
+            this.getFiscalYear = function(date) {
+                date = date || new Date();
+                var year = date.getFullYear(),
+                    month = date.getMonth();
+                if(month >= this.fiscalYearStartMonth)
+                    year++;
+                return year;
+            };
 
-        function nextId() {
-            return ++currentId;
+            this.nextId = function() {
+                return ++this.currentId;
+            };
         }
-    }
-})();
+    ]);
+}();
