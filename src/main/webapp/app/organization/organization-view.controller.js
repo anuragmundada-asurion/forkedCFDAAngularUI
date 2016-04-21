6ff48f48dc2ca2dc760037778619146e065e603d
jahangir-iqbal-rei <jahangir.iqbal@reisystems.com>
2016-04-21 18:27:00 -0400

    update view

--- src/main/webapp/app/organization/organization-view.controller.js
+++ src/main/webapp/app/organization/organization-view.controller.js
@@ -8,16 +8,22 @@
 
             OrganizationFactory.get({id: $stateParams.id}).$promise.then(function (data) {
                 console.log('one org data: ', data);
-                $scope.oOrganization = data;
+                $scope.oOrganization = {
+                    'name': 'GENERAL SERVICES ADMINISTRATION',
+                    'acronym': 'GSA',
+                    'agencyCode': '4700',
+                    'agencyProgramCode': '39',
+                    'programNumberLow': '5',
+                    'programNumberHigh': '100',
+                    'programNumberAuto': true
+                };
 
+                $scope.oOrganization2 = data;
+                console.log('$scope.oOrganization2: ', $scope.oOrganization2);
 
                 FederalHierarchyService.getFederalHierarchyById(data.organizationId, false, false, function (d) {
-                    $scope.oOrganization.name = d.name;
+                    console.log('$scope.oOrganization2: ', $scope.oOrganization2);
                 });
-
-
-
-
 
 
             });
