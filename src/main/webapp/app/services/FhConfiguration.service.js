5ecfa8fdcb3314d5758506eac22e23f8154ba1c1
jahangir-iqbal-rei <jahangir.iqbal@reisystems.com>
2016-04-29 11:55:48 -0400

    parent path

--- src/main/webapp/app/services/FhConfiguration.service.js
+++ src/main/webapp/app/services/FhConfiguration.service.js
@@ -11,10 +11,10 @@
          * @param aSelectedData
          * @returns Object
          */
-        var getFhConfiguration = function (id, callbackSuccess) {
+        var getFhConfiguration = function (idObj, callbackSuccess) {
 
             //factory doesn't return the entire data that i need
-            OrganizationFactory.get(id).$promise.then(function (data) {
+            OrganizationFactory.get(idObj).$promise.then(function (data) {
 
                 //call on fh to get the rest
                 FederalHierarchyService.getFederalHierarchyById(data.organizationId, false, false, function (d) {
@@ -22,8 +22,17 @@
                     data.agencyProgramCode = d.cfdaCode;
                     data.acronym = 'Not available';
                     data.agencyCode = 'Not available';
-                    //execute the callback function
-                    callbackSuccess(data);
+                    //get parent path and put it into string
+                    FederalHierarchyService.getParentPath(idObj.id, function (parentArray) {
+                        var parentPath = '';
+                        parentPath = parentPath + ((parentArray['DEPARTMENT']) ? parentArray['DEPARTMENT'] : '');
+                        parentPath = parentPath + ((parentArray['AGENCY']) ? '/' + parentArray['AGENCY'] : '');
+                        parentPath = parentPath + ((parentArray['OFFICE']) ? '/' + parentArray['OFFICE'] : '');
+                        data.parentPath = parentPath;
+
+                        //execute the callback function
+                        callbackSuccess(data);
+                    });
                 });
             });
 
