e0596f772ec00e6134addec10428c6793bc88efb
Hieu Trang <hieu.trang@reisystems.com>
2016-05-04 12:26:10 -0400

    Added permissions to approve and reject button

--- src/main/webapp/app/request/viewRequest.controller.js
+++ src/main/webapp/app/request/viewRequest.controller.js
@@ -3,31 +3,34 @@
 
     var myApp = angular.module('app');
 
-    myApp.constant('RequestActionMapping', {
-        archive_request: {
-            approve: 'archive',
-            reject: 'archive_reject'
-        },
-        title_request: {
-            approve: 'title',
-            reject: 'title_reject'
-        },
-        agency_request: {
-            approve: 'agency',
-            reject: 'agency_reject'
-        },
-        program_number_request: {
-            approve: 'program_number',
-            reject: 'program_number_reject'
-        },
-        unarchive_request: {
-            approve: 'unarchive',
-            reject: 'unarchive_reject'
-        }
-    });
+    myApp.controller('RequestViewCtrl', ['$stateParams', '$scope', 'RequestFactory', 'Request', 'AuthorizationService', 'PERMISSIONS',
+        function($stateParams, $scope, RequestFactory, Request, AuthorizationService, PERMISSIONS) {
+            $scope.requestMapping = {
+                archive_request: {
+                    approve: 'archive',
+                    reject: 'archive_reject',
+                    permissionRequired: PERMISSIONS.CAN_PERFORM_ARCHIVE
+                },
+                title_request: {
+                    approve: 'title',
+                    reject: 'title_reject',
+                    permissionRequired: PERMISSIONS.CAN_PERFORM_TITLE_CHANGE
+                },
+                agency_request: {
+                    approve: 'agency',
+                    reject: 'agency_reject'
+                },
+                program_number_request: {
+                    approve: 'program_number',
+                    reject: 'program_number_reject'
+                },
+                unarchive_request: {
+                    approve: 'unarchive',
+                    reject: 'unarchive_reject',
+                    permissionRequired: PERMISSIONS.CAN_PERFORM_UNARCHIVE
+                }
+            };
 
-    myApp.controller('RequestViewCtrl', ['$stateParams', '$scope', 'RequestFactory', 'Request', 'RequestActionMapping',
-        function($stateParams, $scope, RequestFactory, Request, RequestActionMapping) {
             $scope.id = $stateParams.id;
 
             RequestFactory.get({id: $scope.id}).$promise.then(function(data){
@@ -55,11 +58,15 @@
             };
 
             $scope.approveRequest = function() {
-                handleRequest(RequestActionMapping[$scope.request.getType()]['approve']);
+                handleRequest($scope.requestMapping[$scope.request.getType()]['approve']);
             };
 
             $scope.rejectRequest = function() {
-                handleRequest(RequestActionMapping[$scope.request.getType()]['reject']);
+                handleRequest($scope.requestMapping[$scope.request.getType()]['reject']);
+            };
+
+            $scope.hasPermissions = function() {
+                return AuthorizationService.authorize(RequestActionMapping[$scope.request.getType()]['permissionRequired']);
             };
 
             function handleRequest(actionType) {
