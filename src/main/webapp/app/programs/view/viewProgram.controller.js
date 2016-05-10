80a8a22c153afb1e6474bbf07660919accb15f5c
jahangir-iqbal-rei <jahangir.iqbal@reisystems.com>
2016-05-10 14:04:33 -0400

    cleaning up controller for public view, removed unneccessary services dependcies

--- src/main/webapp/app/programs/view/viewProgram.controller.js
+++ src/main/webapp/app/programs/view/viewProgram.controller.js
@@ -3,17 +3,14 @@
 
     var app = angular.module('app');
 
-    app.controller('ViewProgramCtrl', ['$state', '$scope', '$stateParams', '$filter', '$parse', 'appConstants', 'SearchFactory', 'ProgramFactory', 'Dictionary', 'appUtil', 'Contact', 'FederalHierarchyService', '$q', '$log', 'ApiService',
-        function ($state, $scope, $stateParams, $filter, $parse, appConstants, SearchFactory, ProgramFactory, Dictionary, appUtil, Contacts, FederalHierarchyService, $q, $log, ApiService) {
+    app.controller('ViewProgramCtrl', ['$state', '$scope', '$stateParams', '$filter', '$parse', 'ProgramFactory', 'Dictionary', 'appUtil', 'FederalHierarchyService', '$q', 'ApiService',
+        function ($state, $scope, $stateParams, $filter, $parse, ProgramFactory, Dictionary, appUtil, FederalHierarchyService, $q, ApiService) {
             $scope.appUtil = appUtil;
-            $scope.$log = $log;
             $scope.programId = $stateParams.id;
             $scope.onPreviewPage = $state.current.data.onPreviewPage;
 
             ProgramFactory.get({id: $stateParams.id}).$promise.then(function (data) {
                 $scope.programData = data;
-
-
                 //getting names of related programs
                 if ($scope.programData['relatedPrograms'] && $scope.programData['relatedPrograms']['flag'] === 'yes') {
                     var promises = [];
@@ -40,7 +37,6 @@
 
 
                 //make call to federalHierarchy
-
                 FederalHierarchyService.getParentPath($scope.programData.organizationId, function (data) {
                     $scope.hierarchyLevels = data;
 
