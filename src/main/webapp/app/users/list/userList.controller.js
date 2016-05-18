4cfc009dce65230e5459c601ac21505ecc65636a
Hieu Trang <hieu.trang@reisystems.com>
2016-05-18 16:38:41 -0400

    Pull organization info from fh for users

--- src/main/webapp/app/users/list/userList.controller.js
+++ src/main/webapp/app/users/list/userList.controller.js
@@ -3,26 +3,13 @@
 
     var myApp = angular.module('app');
 
-    myApp.controller('UserListCtrl', ['$scope', 'ApiService', 'FederalHierarchyService', 'DTColumnBuilder', 'DTOptionsBuilder', 'DTColumnDefBuilder',
-        function($scope, ApiService, FederalHierarchyService, DTColumnBuilder, DTOptionsBuilder, DTColumnDefBuilder) {
+    myApp.controller('UserListCtrl', ['$scope', 'ApiService', 'FederalHierarchyService', 'DTColumnBuilder', 'DTOptionsBuilder', '$q',
+        function($scope, ApiService, FederalHierarchyService, DTColumnBuilder, DTOptionsBuilder, $q) {
             $scope.searchFirstName = '';
             $scope.searchLastName = '';
             $scope.dtInstance = {};
 
-            $scope.dtOptions = DTOptionsBuilder.fromFnPromise(function() {
-                    var oApiParam = {
-                        apiName: 'userList',
-                        apiSuffix: '',
-                        oParams: {},
-                        oData: {},
-                        method: 'GET'
-                    };
-
-                    if ($scope.searchFirstName || $scope.searchLastName) {
-                    }
-
-                    return ApiService.call(oApiParam);
-                })
+            $scope.dtOptions = DTOptionsBuilder.newOptions()
                 .withOption('infoCallback', function(settings, start, end, max, total, pre) {
                     $scope.totalCount = total;
                 })
@@ -36,6 +23,40 @@
                     $(".dataTables_info").contents().unwrap();
                 })
                 .withOption('processing', true)
+                .withOption('ajax', function(data, callback, settings) {
+                    var oApiParam = {
+                        apiName: 'userList',
+                        apiSuffix: '',
+                        oParams: {},
+                        oData: {},
+                        method: 'GET'
+                    };
+
+                    if ($scope.searchFirstName || $scope.searchLastName) {
+                    }
+
+                    ApiService.call(oApiParam).then(
+                        function (results) {
+                            var promises = [];
+                            var tableData = [];
+                            angular.forEach(results, function (r) {
+                                if (r['orgID']) {
+                                    promises.push(FederalHierarchyService.getFederalHierarchyById(r['orgID'], true, false, function (data) {
+                                        r['organization'] = FederalHierarchyService.getFullNameFederalHierarchy(data);
+                                    }, function () {
+                                        r['organization'] = 'Organization Not Found';
+                                    }));
+                                } else {
+                                    r['organization'] = 'No Organization Assigned';
+                                }
+                                tableData.push(r);
+                            });
+                            $q.all(promises).then(function () {
+                                callback(tableData);
+                            });
+                        }
+                    );
+                })
                 .withOption('searching', false)
                 .withOption('lengthMenu', [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]])
                 .withDOM('<"top ui fixed container"r> <"ui fixed container"t> <"bottom background gray" <"ui fixed container" <"ui grid" <"two column row" <"column"li> <"column"p> > > > > <"clear">')
@@ -55,7 +76,7 @@
                 DTColumnBuilder.newColumn('organization').withTitle('Department/Sub-Tier Agency & Office').withOption('defaultContent', ''),
                 //DTColumnBuilder.newColumn('assignedAgencies').withTitle('Assigned Agencies').withOption('defaultContent', ''),
                 DTColumnBuilder.newColumn('email').withTitle('E-mail').withOption('defaultContent', ''),
-                DTColumnBuilder.newColumn('phone').withTitle('Phone').withOption('defaultContent', ''),
+                DTColumnBuilder.newColumn('workPhone').withTitle('Phone').withOption('defaultContent', ''),
                 DTColumnBuilder.newColumn('_id').withTitle('Username').withOption('defaultContent', ''),
                 //DTColumnBuilder.newColumn('permissions').withTitle('Permission Attributes').withOption('defaultContent', ''),
                 //DTColumnBuilder.newColumn('lastLogin').withTitle('Last Login').withOption('defaultContent', ''),
