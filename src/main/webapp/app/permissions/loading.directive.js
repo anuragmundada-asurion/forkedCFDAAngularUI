3af7a07757b6a7bdb8803592dc7d09a1e311a206
Carlos Valle <carlos.m.valle@gmail.com>
2016-06-14 16:33:40 -0400

    CFDA-1136: UX improvements

--- src/main/webapp/app/permissions/loading.directive.js
+++ src/main/webapp/app/permissions/loading.directive.js
@@ -7,7 +7,7 @@
         return {
             restrict: 'E',
             replace: true,
-            template: '<div style="width: 100%;height: 100%;z-index: 10000;top: 0;left: 0;opacity: .75;position: fixed;background: black center;"><div style="background: url(\'/img/img_cfda/loading_white.svg\') no-repeat center;width: 100%;height: 100%;"></div></div>',
+            template: '<div style="width: 100%;height: 100%;z-index: 10000;top: 0;left: 0;opacity: .75;position: fixed;background: black center;"><div class="ui active dimmer"><div class="ui large loader"></div></div></div>',
             link: function(scope, element) {
                 scope.$watch(
                     function() {
