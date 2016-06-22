15dd5a145b648c200d1def006ee5b7fc06efab79
Hieu Trang <hieu.trang@reisystems.com>
2016-06-22 18:22:08 -0400

    Disable cache only for IE

--- src/main/webapp/app/app.config.js
+++ src/main/webapp/app/app.config.js
@@ -72,15 +72,20 @@
     ]);
 
     myApp.config(['$httpProvider', function($httpProvider) {
-        //  initialize get if not there
-        if (!$httpProvider.defaults.headers.get) {
-            $httpProvider.defaults.headers.get = {};
-        }
+        var ua = window.navigator.userAgent;
+        var msie = ua.indexOf("MSIE ");
 
-        //  disable IE ajax request caching
-        $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
-        //  extra
-        $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
-        $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+        if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) {
+            //  initialize get if not there
+            if (!$httpProvider.defaults.headers.get) {
+                $httpProvider.defaults.headers.get = {};
+            }
+
+            //  disable IE ajax request caching
+            $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
+            //  extra
+            $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+            $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+        }
     }]);
 })();
