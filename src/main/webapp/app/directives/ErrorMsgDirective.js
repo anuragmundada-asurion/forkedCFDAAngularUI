a4cc248034aeb655d21e2a31df014d1670e59689
anurag.mundada <anurag.mundada@reisystems.com>
2016-06-23 13:05:23 -0400

    Bug fix for Website url and for all similar fields

--- src/main/webapp/app/directives/ErrorMsgDirective.js
+++ src/main/webapp/app/directives/ErrorMsgDirective.js
@@ -20,7 +20,7 @@
                     if (newValue) {
                         if(scope.regexPattern == "website_url"){
                             //use literal for website url regex
-                            if(!(/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/.test(newValue))){
+                            if(!(/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/.test(newValue)) && !(scope.model.length == 0)){
                                 scope.showErrorMsg = true;
                                 element.closest(".input-field").addClass("usa-input-error");
                             }else{
@@ -37,6 +37,10 @@
                             }
                         }
 
+                    }
+                    if(scope.model.length == 0){
+                        scope.showErrorMsg = false;
+                        element.closest(".input-field").removeClass("usa-input-error");
                     }
                 });
 
