8746b0563cb0385e8b7944800918ae811680ecf8
Hieu Trang <hieu.trang@C02RV0R8G8WL.local>
2016-06-29 17:34:44 -0400

    Removed GSA Analyst from supported roles and added role definitions to rootScope

--- src/main/webapp/app/permissions/role.service.js
+++ src/main/webapp/app/permissions/role.service.js
@@ -8,7 +8,6 @@
         AGENCY_COORDINATOR: 'GSA_CFDA_R_cfda_agency_coord',
         AGENCY_USER: 'GSA_CFDA_R_agency_submitter',
         OMB_ANALYST: 'GSA_CFDA_R_omb_analyst',
-        GSA_ANALYST: 'GSA_CFDA_R_gsa_analyst',
         RMO_SUPER_USER:'GSA_CFDA_R_rmo_superuser',
         LIMITED_SUPER_USER: 'GSA_CFDA_R_cfdalimitedsuperuser'
     });
@@ -36,12 +35,16 @@
                 var roleList = {};
                 angular.forEach(SUPPORTED_ROLES, function(v) {
                     var permissionList = [];
+                    var name = '';
                     data.some(function(r) {
                         if (angular.equals(r['roleId'], v)) {
+                            name = r['name'];
                             buildPermissionList(r, permissionList);
+                            return true;
                         }
                     });
                     roleList[v] = {
+                        name: name,
                         permissions: permissionList
                     };
                 });
@@ -52,8 +55,11 @@
         }
     ]);
 
-    myApp.run(['$rootScope', 'SUPPORTED_ROLES', function($rootScope, SUPPORTED_ROLES) {
-        $rootScope.ROLES = SUPPORTED_ROLES;
+    myApp.run(['$rootScope', 'SUPPORTED_ROLES', 'ROLES', function($rootScope, SUPPORTED_ROLES, ROLES) {
         $rootScope.SUPPORTED_ROLES = SUPPORTED_ROLES;
+        $rootScope.ROLE_DEFINITIONS = ROLES;
+
+        //  TODO: Remove and use SUPPORTED ROLES in it's place
+        $rootScope.ROLES = SUPPORTED_ROLES;
     }]);
 }();
