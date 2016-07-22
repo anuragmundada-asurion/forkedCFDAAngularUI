c7a058990ba573ccd4f4f9b2a9cf03c3344bed73
JihadMotii-REISys <jihad.motii@reisystems.com>
2016-07-22 16:12:26 -0400

    Adding unit tests

--- src/test/javascript/spec/organization/organization.controller.spec.js
+++ src/test/javascript/spec/organization/organization.controller.spec.js
@@ -39,9 +39,17 @@ describe('Unit Tests for Organization View Controller:', function () {
             $httpBackend
                 .whenGET('/v1/federalHierarchy?sort=name')
                 .respond({});
+
+            $httpBackend
+                .whenGET('/v1/federalHierarchy?childrenLevels=all&parentLevels=all&sort=name')
+                .respond({
+                    _links:{},
+                    type: "",
+                    elementId: ""
+                });
         });
 
-        it('should initialize variables', function () {
+        it('should initialize variables (View)', function () {
             $stateParams.id = 1;
             $controller('OrganizationViewCtrl', {$scope: scope, $stateParams: $stateParams});
 
@@ -49,6 +57,37 @@ describe('Unit Tests for Organization View Controller:', function () {
             expect(scope.id).toBe($stateParams.id);
             $httpBackend.flush();
             expect(scope.oOrganization).toBeDefined();
+        });
+
+        it('should initialize variables (List)', function () {
+            $stateParams.id = 1;
+            $controller('OrganizationListController', {$scope: scope});
+
+            expect(scope.searchKeyword);
+            scope.dtInstance = { DataTable: {ajax: {reload: function(){}}} };
+            $httpBackend.flush();
+
+            expect(scope.dtData_topLevel).toBeDefined();
+            expect(scope.dtData_total).toBeDefined();
+            expect(scope.childrenMap).toBeDefined();
+            expect(scope.dtData).toBeDefined();
+            expect(scope.loadOrganizations).toBeDefined();
+            expect(scope.rowClicked).toBeDefined();
+            expect(scope.dtInstance).toBeDefined();
+            expect(scope.dtColumnDefs).toBeDefined();
+            expect(scope.dtOptions).toBeDefined();
+            expect(scope.dtColumns).toBeDefined();
+        });
+
+        it('should initialize variables (Form)', function () {
+            $stateParams.id = 1;
+            $controller('OrganizationFormCtrl', {$scope: scope, $stateParams: $stateParams});
+
+            expect(scope.items);
+            expect(scope.selected);
+            expect(scope.saveFhConfiguration);
+            $httpBackend.flush();
+            expect(scope.oOrganization);
         });
     });
 });
